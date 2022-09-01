3b1bfcbd2f44890489fc714df05cc04ce97bc264
Saman-Dakheel <92514384+Saman-Dakheel@users.noreply.github.com>
2022-09-01 19:11:26 +0300

    Add files via upload

---
+++ 3-set/main.py
@@ -0,0 +1,68 @@
+# food = {"apple", "banana" , "cherry" , 1,2,6}
+# print(food)
+
+# food = {"apple", "banana" , "cherry"}
+# print(food)
+
+# person = {'adel', 'bahzani' , 'dler'}
+#
+# print("adel" in person)
+
+# person = {'adel', 'bahzani' , 'dler',"talib"}
+# print(person)
+# print(type(person))
+
+
+# grades = {23,55,66,77}
+# grades.add(99)
+# grades.add(9)
+# print(grades)
+
+
+# cars = {"Huenda" , "BMW", "Marceds"}
+# cars1 = {"Faw" , "Kai" , "Opel"}
+# cars.update(cars1)
+# print(cars2)
+#
+# cars = {"Huenda" , "BMW", "Marceds"}
+# cars1 = {"Faw" , "Kai" , "BMW"}
+# cars2 = cars.union(cars1)
+# print(cars2)
+
+
+# cars = {"Huenda" , "BMW", "Marceds","lamborghini"}
+# print(cars)
+# cars.remove("lamborghini")
+# print(cars)
+
+# cars = {"Huenda" , "BMW", "Marceds","lamborghini"}
+# cars.clear()
+# print(cars)
+
+#
+# cars = {"Huenda" , "BMW", "Marceds","lamborghini"}
+# cars1 = {"Huenda" , "kai", "faw","lamborghini"}
+#
+# cars2=cars.difference(cars1)
+# print(cars2)
+
+# cars = {"Huenda" , "BMW", "Marceds","lamborghini"}
+# cars1 = {"Huenda" , "kai", "faw","lamborghini"}
+# cars.difference_update(cars1)
+# print(cars)
+
+# cars = {"Huenda" , "BMW", "Marceds","lamborghini"}
+# cars1 = {"Huenda" , "kai", "faw","lamborghini"}
+# cars2 = cars.intersection(cars1)
+# print(cars2)
+
+
+#
+# cars = {"Huenda" , "BMW", "Marceds","lamborghini"}
+# cars1 = {"Huenda" , "kai", "faw","lamborghini"}
+# cars.intersection_update(cars1)
+# print(cars)
+
+
+
+
