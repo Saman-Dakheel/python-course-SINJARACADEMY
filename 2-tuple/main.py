5874853649ce252a53100421539665d802b5d30c
Saman-Dakheel <92514384+Saman-Dakheel@users.noreply.github.com>
2022-09-01 19:10:37 +0300

    Add files via upload

---
+++ 2-tuple/main.py
@@ -0,0 +1,40 @@
+# new_tuple= ( 1,2,3,4,5)
+# new_tuple1= (4,9,5,6.98,'saman',[1,2,3,4])
+# print(new_tuple)
+# print(new_tuple1)
+
+# new_tuple = ( 1,2,3,4,5)
+# print(type(new_tuple))
+
+
+# t1= (0,1,2,3,34,4,8)
+# print(len(t1))
+
+
+# t1= (0,1,2,3,34,4,8)
+# t1[2]= 33
+# print(t1[2])
+
+# t1= (0,1,2,3,34,4,8)
+# print(t1[-1:])
+
+
+# t1 = (12,3,4,5)
+# x = list(t1)
+# x.append(12)
+# print(x)
+
+
+# t1 = (1,2,3)
+# t2 = (4,5,6)
+# t3 = t1 + t2
+# print(t3)
+
+
+# t1 = (2,3,3,4,5,5,6,6,6)
+# x = t1.count(4)
+# print(x)
+
+# t1 = (2,3,3,4,5,5,6,6,6)
+# x = t1.index(6)
+# print(x)
