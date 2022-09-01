a49634f06cd1f8b1a1f0e3d62fe6335e880bda63
Saman-Dakheel <92514384+Saman-Dakheel@users.noreply.github.com>
2022-09-01 19:10:00 +0300

    Add files via upload

---
+++ 1-list/main.py
@@ -0,0 +1,53 @@
+# list1 = [1 , 2 ,3,4 ,5]
+# print(list1[0:3])
+# print(list1[-3:-1])
+# print(list1[-4:])
+# print(list1[-3:5])
+
+# fruits= ['apple' , 'banana' , 'orange']
+# print(fruits)
+# fruits.append('pineapple')
+# print(fruits)
+
+
+
+# fruits = ['apple' , 'banana' , 'orange']
+# print(fruits)
+# fruits.extend(['Tommato' , 'pottato'])
+# print(fruits)
+
+# fruits = ['apple' , 'banana' , 'orange']
+# print(fruits)
+# fruits.reverse()
+# print(fruits)
+
+# fruits = ['apple' , 'banana' , 'orange']
+# del fruits[2]
+# print(fruits)
+
+
+# fruits = ['apple' , 'banana' , 'orange']
+# fruits.pop(1)
+# print(fruits)
+
+# fruits = ['apple' , 'banana' , 'orange']
+# fruits.clear()
+# print(fruits)
+
+# fruits = ['apple' , 'banana' , 'orange']
+# print(fruits)
+# new_list = fruits.copy()
+# new_list.append('pineapple')
+# print(fruits)
+# print(new_list)
+
+# fruits = ['apple' , 'banana' , 'orange']
+# print(fruits)
+# new_list = fruits
+# new_list.append('pineapple')
+# print(fruits)
+# print(new_list)
+
+# fruits = ['apple' , 'banana' , 'orange' , 'banana' , 'orange' , 'banana' , 'orange' , 'banana' , 'orange' ]
+# x = fruits.count('bananaa')
+# print(x)
