ee7770839312c71d82dd513c0dcd4b853fe91f39
Saman-Dakheel <92514384+Saman-Dakheel@users.noreply.github.com>
2022-09-04 22:16:05 +0300

    Add files via upload

---
+++ 5-input/main.py
@@ -0,0 +1,72 @@
+#
+# x = input("Enter Your Name: ")
+# print( "Your name is " + x)
+
+#
+# name = input("Enter Your Name: ")
+# print( f"Your name is {name}" )
+
+
+# name = input("Enter Your Name: ")
+# age = input("Enter Your age ")
+# print( f"Your name is {name} your age is {age}" )
+
+
+# name = input("Enter Your Name: ")
+# age = int(input("Enter Your age "))
+# age = age + 2
+# print( f"Your name is {name} "
+#        f"after 2 year your age will be {age}" )
+
+
+# name = str(input("Enter Your Name: "))
+# age = float(input("Enter Your age "))
+# age = age + 2
+# print( f"Your name is {name} "
+#        f"after 2 year your age will be {age}" )
+
+
+# name = str(input("Enter Your Name: "))
+# # age = int(input("Enter Your age "))
+# # age = age + 2
+# # print( f"Your name is {name.capitalize()} "
+# #        f"after 2 year your age will be {age}" )
+
+# name = str(input("Enter Your Full Name: "))
+# age = int(input("Enter Your age "))
+#
+# print( f"Your name is {name.title()}"
+#        f"after 2 year your age will be {age}" )
+
+
+
+# name = str(input("Enter Your Name: "))
+# age = int(input("Enter Your age "))
+# age = age + 2
+# print( f"Your name is {name.strip()} "
+#        f"after 2 year your age will be {age}" )
+
+# name = str(input("Enter Your Full Name: "))
+# age = int(input("Enter Your age "))
+# name = name.title()
+# name= name.capitalize()
+# name = name.strip()
+# print( f"Your name is {name} "
+#        f"after 2 year your age will be {age}" )
+
+
+
+# price = int(input("Enter price "))
+# piece = int(input("Number of piece "))
+# cost = int(input("enter cost "))
+#
+# total = price * piece - cost
+# print(f"Today you get {total}")
+
+
+
+price = float(input("Enter price "))
+piece = float(input("Number of piece "))
+cost = float(input("enter cost "))
+total = price * piece - cost
+print(f"Today you get {round(total)}")
