67c35b84de1d00f039fbc4305d9ffe466b043bad
Saman-Dakheel <92514384+Saman-Dakheel@users.noreply.github.com>
2022-09-06 17:21:29 +0300

    Add files via upload

---
+++ 6-strings/main.py
@@ -0,0 +1,124 @@
+# string = '''Sinjar Academy Provide
+# course online and onsite
+# and its location in sinjar  snuny'''
+#
+# print(string)
+
+
+# x = "saman"
+# print(x[0:3])
+#
+# x = "saman"
+# print(x[-3:-1])
+
+
+# x = "khery is here"
+#
+# print("s" in x)
+
+# x = "khery is here"
+# y = "khery1" in x
+# print(y)
+
+# x = "khery is here"
+# print("khery2" not in x)
+
+
+# x = " khery is here "
+#
+# print(len(x))
+
+
+# x = "khery Is Here"
+#
+# print(x.upper())
+
+# x = "khery is here".upper()
+# print(x)
+
+
+# x = "  khery is here"
+# print(x.strip())
+
+
+
+# x = "khery is here"
+# print(x.replace("khery" , "ghada"))
+
+# x = "khery is khery here"
+# print(x.replace("khery" , "ghada"))
+
+
+# x = "khery is here"
+# print(x.split(" "))
+#
+# x = "khery is here"
+# print(x.split("e"))
+
+
+# x = "saman"
+# y = "dakheel"
+# z= x+' '+y
+# print(z)
+
+#
+# fname= "aiser"
+# lname = "eido"
+# age = 70
+# print("your first name is {} and your second name is {} your age is {}".format(fname , lname, age))
+
+
+# age = 22
+# work=" and I am a learning Python"
+# txt = "My name is khery, I am {},{}"
+# print(txt.format(age,work))
+
+
+# x='it\'s my language'
+# print(x)
+
+# x="iT`s my language"
+# print(x)
+
+
+
+# x =" sinjar city sinjar is here "
+# print(x.count("x"))
+
+
+# x ="sinjar city sinjar is here"
+# print(x.endswith("here"))
+
+# x ="sinjar city sinjar is here"
+# print(x.startswith("sinjar"))
+
+#
+# x ="city sinjar is here"
+# print(x.find("i"))
+
+
+# x  = "8"
+# print(x.isdigit())
+# y = float(x)+4
+# print(y)
+
+
+# x = "saman dakheel"
+# print(x.title())
+
+# x = "saman dakheel"
+# y = x.maketrans('a' , 'd')
+# print(x.translate(y))
+
+# x = "saman dakheel"
+#
+# print(x.maketrans('z' , 'Z'))
+
+# x =ascii(97)
+# print(str(x))
+
+
+
+
+
+
