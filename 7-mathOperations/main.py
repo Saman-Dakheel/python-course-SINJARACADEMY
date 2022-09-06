973ffe8f61b1c87fb3dab82612c171d957d8dadc
Saman-Dakheel <92514384+Saman-Dakheel@users.noreply.github.com>
2022-09-06 22:02:32 +0300

    Add files via upload

---
+++ 7-mathOperations/main.py
@@ -0,0 +1,103 @@
+# x = 1
+# y = 2
+# z=x+y
+# print(x+y)
+
+
+
+# x = 1
+# y = 2
+# z=x-y
+# print(z)
+
+# x = 8
+# y = 2
+# z = x / y
+# print(z)
+
+
+# x = 2
+# y = 8
+# z = x / y
+# print(z)
+
+
+# x = 8
+# y = 2
+# z = x / y
+# print(int(z))
+
+
+# x = 2
+# y = 8
+# z = x * y
+# print(z)
+
+# x = 2
+# y = 8
+# z = x * y
+# print(float(z))
+
+
+
+
+# p = 3 / 30
+# per = p * 100
+# print(per)
+
+
+# x = 2
+# y = 4
+# z = x ** y # 2*2*2*2
+# print(z)
+
+#
+# x =19
+# y = 2
+# z = x // y  # floor to small number
+# print(z)
+
+
+# x =19
+# y = 3
+# z = x / y  # floor to small number
+# print(round(z))
+
+
+
+
+#
+# x =19
+# y = 3
+# z = x / y  # floor to small number
+# print(z.__floor__())
+
+
+# import math as m
+# x =19
+# y = 3
+# z = x / y  # floor to small number
+# print(m.floor(z))
+
+
+# import math as m
+# x =19
+# y = 3
+# z = x / y  # floor to small number
+#print(m.ceil(z))
+
+
+# x = 7
+# y = 5
+# print(x == y)
+
+# x = 7
+# y = 8
+# print(x != y)
+
+x = 7
+y = 5
+print(x < y)  #false
+print(x > y)   #T
+print(x <= y)  #F
+print(x >= y)  # T
