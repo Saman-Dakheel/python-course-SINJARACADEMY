acb845cfc5f1ff17df19140ffe1ef7e3e559fcd2
Saman-Dakheel <92514384+Saman-Dakheel@users.noreply.github.com>
2022-09-13 19:19:30 +0300

    Add files via upload

---
+++ 10-conitional2/main.py
@@ -0,0 +1,62 @@
+# x = int(input("Enter a number: "))
+# x = x % 2
+# if x == 0:
+#     print("Number is even")
+# else:
+#     print("Number is odd")
+
+
+# x = int(input("Enter a number: "))
+# if x % 2 == 0:
+#     print("Number is even")
+# else:
+#     print("Number is odd")
+
+#
+# grade  = int(input("Enter Student Grade: "))
+# if grade > 90 and grade <= 100:
+#     print("Student is Excellent ")
+# elif grade > 80 and grade <= 90:
+#     print("Student is Very good ")
+# elif grade > 70 and grade <=80:
+#     print("Student is good ")
+# elif grade > 60 and grade <=70 :
+#     print("Student is Medium")
+# elif grade >= 50 and grade <= 60:
+#     print("Student is Pass")
+# elif grade >= 0 and grade < 50 :
+#     print("Student is Failed")
+# else:
+#     print("Number out of range Enter number between 0 to 100")
+#
+
+#
+# grade = int(input("Enter Student Grade: "))
+# if 90 < grade <= 100:
+#     print("Student is Excellent ")
+# elif 80 < grade <= 90:
+#     print("Student is Very good ")
+# elif 70 < grade <= 80:
+#     print("Student is good ")
+# elif 60 < grade <= 70:
+#     print("Student is Medium")
+# elif 50 <= grade <= 60:
+#     print("Student is Pass")
+# elif 0 <= grade < 50:
+#     print("Student is Failed")
+# else:
+#     print("Number out of range Enter number between 0 to 100")
+
+
+# Profit Calculator
+# expect = int(input("Enter Expect profit: "))
+# products = int(input("Enter sold products: "))
+# price = int(input("Enter sold price: "))
+# cost = int(input("Enter cost: "))
+# profit = (products * price)
+# if profit < cost:
+#     print("You lose money")
+# else:
+#     print("You win money ")
+
+
