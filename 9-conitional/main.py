74d617243fcc7826b4e2a931689374eea2222f58
Saman-Dakheel <92514384+Saman-Dakheel@users.noreply.github.com>
2022-09-13 19:18:54 +0300

    Add files via upload

---
+++ 9-conitional/main.py
@@ -0,0 +1,74 @@
+'''
+>
+<
+>=
+<=
+==
+!=
+'''
+# x = 13
+# y = 14
+# if x != y:
+#     print("welcome three")
+#     print("gggggggg")
+# if y > x:
+#     print("Welcome seven")
+
+# x = 16
+# y = 14
+# if y > x:
+#     print(f"{y} is greater than {x}")
+# if y < x:
+#     print(f"{x} is greater than {y}")
+
+
+# x , y = 9 , 8
+# if x < y:
+#     print("x is greater than y")
+# if x < y:
+#     print("x is greater than y")
+# else:
+#     print("condition is not true")
+
+# x= 88
+# y = 9
+# if x > y:
+#     print(y)
+# elif x < y:
+#     print("x < y")
+# else:
+#     print("Not ")
+
+# x = 5
+# y = 4 # true false = false
+# z = 9 # false true = false # false false = false
+# if x < y > z :
+#     print("True")
+# else:
+#     print("false")
+
+
+# and
+# True True =True
+# false false = false
+# false true  = false
+# x  =9
+# y = 8
+# z = 7
+# if x < y and z < y:
+#     print("true ")
+# else:
+#     print("false")
+
+# or
+# True True =True
+# false false = false
+# false true  = true
+# true false = true
+x = 9
+y = 8
+z = 7
+if x < y or z < y:
+    print("true")
+else:
+    print("false")
