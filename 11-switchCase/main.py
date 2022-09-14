a2ea9ed9b3ad926e010a19102d6892a51e873fd4
Saman-Dakheel <92514384+Saman-Dakheel@users.noreply.github.com>
2022-09-14 18:30:41 +0300

    Add files via upload

---
+++ 11-switchCase/main.py
@@ -0,0 +1,45 @@
+# num = int(input('number : '))
+# # Switch
+# match num:
+#     case 1:
+#         print("one")
+#     case 2:
+#         print("tow")
+#     case 3:
+#         print("three")
+
+
+# fav_team = input("Type Your favorite team name : ")
+# match fav_team:
+#     case "real":
+#         print("Real Madriad is the king ")
+#     case "byrin":
+#         print("bavary ")
+#     case "Barshlona":
+#         print("barsa")
+#     case "city":
+#         print("")
+#     case "united":
+#         print("Ronaldo")
+#     case "psg":
+#         print("Paris san jirman ")
+#     case _:
+#         print("Your team is not here ")
+
+
+# fav_team = input("Type Your favorite team name : ")
+# if fav_team == "real":
+#     print("Real Madriad is the king ")
+# elif fav_team == "barsa":
+#     print("barsa")
+# else:
+#     print("Your team is not here ")
+
+
+integer = int(input("number: "))
+match integer:
+    case 99:
+        print("90 ")
+    case [1,2 ,3,4,5,6,7,8,9,0]:
+        print("welcome")
+
