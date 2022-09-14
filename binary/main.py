bcce79e6d1cee3bdc225e092d046af8eb6bb4222
Saman-Dakheel <92514384+Saman-Dakheel@users.noreply.github.com>
2022-09-14 22:14:55 +0300

    Add files via upload

---
+++ binary/main.py
@@ -0,0 +1,42 @@
+# Binary & Decimal & & octal & hexadecimal & Ascii code
+# binary 0 1
+# decimal 0-9
+# Octal  0 1 2 3 4 5 6 7
+# hexadecimal 0 9 a f
+# 16 8 4 2 1
+# 1  0 0 1 1
+
+
+# Binary to decimal
+# x = int("110101",2)
+# print(x)
+
+
+# Decimal to Binary
+# x =format(22,'b') # o for octal x for hexadecimal
+# print(x)
+
+
+# hexadecimal
+# y = hex(22)
+#x =y.replace("0x" , "")
+# print(y)
+
+# octal
+# x = oct(22)
+# print(x)
+
+# Another way to do it
+# The bin() function returns the binary version of a specified integer.
+# The result will always start with the prefix 0b.
+# x = bin(5)
+# y = x.replace("0b" ,"")
+# print(y)
+
+
+
+# ascii code
+# c = '$'
+# print("The ASCII code of '" + c + "' is", ord(c))
+# c =8
+# print("The ASCII value of '" + str(c) + "' is " +chr(c))
