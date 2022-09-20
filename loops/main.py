e1d13471186eda88f004dd023c70eab2f25e5a79
Saman-Dakheel <92514384+Saman-Dakheel@users.noreply.github.com>
2022-09-20 20:18:52 +0300

    Add files via upload

---
+++ loops/main.py
@@ -0,0 +1,95 @@
+# list1= [12 ,2,3,4,5]
+# for value in list1:
+#   if value >= 3:
+#     value = value + 5
+#     print(value)
+# print("finish")
+
+
+# users = ['elias', "fuaad", 'ghazal', "gule", "haji"]
+# for i in users:
+#     print(i.upper())
+
+
+# numbers = [1, 2, 3, 4, 55, 5, 6, 7, 7, 78, 8, 9]
+# sum = 1
+# for item in numbers:
+#     # 10   10     5
+#     sum = sum * item
+#     print(sum)
+# print(sum)
+
+
+# name = input("Enter your name : ")
+# n = ""
+# for i in name:
+#     n='it\'s' + n+i # \n \t 4 space \"
+# print("Welcome " + n)
+
+
+
+
+# list1 =[ ' saman' , 'farhaad ' , 'saad', 'ameer']
+# for name in list1:
+#     print(name)
+
+
+# list1 = [1,2,3,4,5,6,7]
+# list2 = [1,2,3,4,5,6,7]
+# for i in range(10): # 0 1  2 3 4 5
+#     print(i)
+
+
+# for i in range(3,8):
+#     print(i)
+
+
+# for i in range(22,28):
+#     print(i)
+
+# for i in range(0, 101, 2):
+#     print(i)
+
+
+
+# for i in range(1, 30, 2):
+#     print(i)
+
+# i = int(input("number : ")) # 2n (n+1)
+# if i % 2 != 0:
+#     print("فردي")
+# else:
+#     print("زوجي")
+
+
+# for i in range(300):
+#     print(i)
+#     if i == 3:
+#         break
+# else:
+#     print("message")
+
+# i = 0
+# for i in 8:
+#     print(i)
+#     i += 1
+
+
+# i = 4
+# while i <= 8:
+#     print(i)
+#     i += 1
+
+
+
+# while True:
+#     name = input("name : ")
+#     print("welcome {}".format(name))
+#     break
+
+
+
+# did = {"key1":'saman',"key2":'manji', "key3":"nazi", "key4":'saher',"key5":'saad',
+# }
+# for i in range(1,6):
+#     print(did[f'key{i}'])
