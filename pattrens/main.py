698dcd7ae0179067d2284a8763329531b3a3de3f
Saman-Dakheel <92514384+Saman-Dakheel@users.noreply.github.com>
2022-09-22 22:26:21 +0300

    Update main.py

--- pattrens/main.py
+++ pattrens/main.py
@@ -8,27 +8,75 @@
 #         print("* ", end='')
 #     print()
 
-row = 5
-column = 0
-for i in range(1 , row+1):
-    for j in range(1, (row-i)+1):
-        print(end=' ')
-    while  column != (2*i - 1):
-        print("*", end='')
-        column = column +1
-    column = 0
-    print()
-column = 2
-m = 1
-for i in range(1,row):
-    for j in range(1, column):
-        print(end=' ')
-    column += 1
-    while m <= (2 * (row - i)- 1):
-        print('*' , end='')
-        m=m+1
-    m=1
-    print()
+
+
+
+# row = 5
+# column = 0
+# for i in range(1 , row+1):
+#     for j in range(1, (row-i)+1):
+#         print(end=' ')
+#     while  column != (2*i - 1):
+#         print("*", end='')
+#         column = column +1
+#     column = 0
+#     print()
+# column = 2
+# m = 1
+# for i in range(1,row):
+#     for j in range(1, column):
+#         print(end=' ')
+#     column += 1
+#     while m <= (2 * (row - i)- 1):
+#         print('*' , end='')
+#         m=m+1
+#     m=1
+#     print()
+    
+    
+    
+# program to identify the vowel letters
+#
+# vowel_letters = "aeiouAEIOU"
+#
+# while True:
+#    i = input("Enter a vowel letter: ")
+#    if i in vowel_letters:
+#        break
+#    print("That is not a vowel letter. Try again!")
+#
+# print("Coorect!", i , "is a vowel letter")
+
+
+
+
+
+# for i in range(0, 5):
+#     for j in range(0, i + 1):
+#         print("*", end=' ')
+#     print()
+#
+# for i in range(5, 0, -1):
+#     for j in range(0, i - 1):
+#         print("*", end=' ')
+#     print()
+#
+
+
+
+# y=10
+# for i in range(0,y) :
+#     for j in range(i+1) :
+#         print (end='* ')
+#     print()
+# x=9
+# for i in range(x, 0, -1):
+#     for j in range(i, 0, -1):
+#         print(end='* ')
+#     print()
+
+
+
 
 # 21 KOTELK
 # 0 1 1 2 3 5 8 13 21
